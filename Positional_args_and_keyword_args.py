63a26811e53533a456a2b8dd2c8dadb2ffd38791
aqib-mehmood <aqimehmoodb@gmail.com>
2020-10-01 00:20:52 +0500

    modified positional and keyword args

--- Positional_args_and_keyword_args.py
+++ Positional_args_and_keyword_args.py
@@ -2,6 +2,6 @@ def add(a, b, c):
   print("The sum of the three numbers is", a + b + c)
 
 add(5, 10, 15)
-add(a = 5, b = 10, c = 15)
+add(a = 15, b = 5, c = 10)
 add(5, b = 10, c = 15)
-add(5, c = 15, b = 10)
+add(10, c = 5, b = 15)
